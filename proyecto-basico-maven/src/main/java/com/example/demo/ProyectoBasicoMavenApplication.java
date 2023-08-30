4c48ba99119d9996d7882c78f0b7f1acc99c40d6
xaco04 <xavivico04@gmail.com>
2023-08-31 00:22:27 +0200

    Exercicis fets

---
+++ proyecto-basico-maven/src/main/java/com/example/demo/ProyectoBasicoMavenApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProyectoBasicoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBasicoMavenApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
+
+
+
+
+
+
